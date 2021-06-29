368daab4f41c9e1b89ad1b9df5dc7d6d2ae78a51
sushantbasak <sushantbasak73@gmail.com>
2021-06-29 22:58:55 +0530

    Add View Delete Update a Upload

--- src/controllers/upload.js
+++ src/controllers/upload.js
@@ -19,7 +19,7 @@ const createUpload = async (req, res) => {
 
 const getUploadAll = async (req, res) => {
   const uploadQuery = `
-  select * from uploads where user_id = $1`;
+  select upload_id,project_name from uploads where user_id = $1`;
   try {
     const uploads = await pool.query(uploadQuery, [req.user.user_id]);
 
@@ -44,4 +44,72 @@ const deleteUploadAll = async (req, res) => {
   }
 };
 
-module.exports = { getUploadAll, createUpload, deleteUploadAll };
+const viewUpload = async (req, res) => {
+  const uploadQuery = `
+  select * from uploads where upload_id = $1`;
+  try {
+    const upload = await pool.query(uploadQuery, [req.params.id]);
+
+    if (upload.rows.length === 0) throw new Error();
+
+    res.send(upload.rows[0]);
+  } catch (e) {
+    res.send('Unable to view Upload');
+  }
+};
+
+const deleteUpload = async (req, res) => {
+  const uploadQuery = `
+  delete from uploads where upload_id = $1 and user_id = $2 returning *;`;
+
+  try {
+    const upload = await pool.query(uploadQuery, [req.params.id, req.user.user_id]);
+
+    if (upload.rows.length === 0) throw new Error();
+
+    res.send(upload.rows[0]);
+  } catch (e) {
+    res.send('Unable to Delete the Upload');
+  }
+};
+
+const updateUpload = async (req, res) => {
+  const updates = Object.keys(req.body);
+
+  const allowedUpdates = ['projectName', 'description'];
+
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid Update operation' });
+  }
+
+  updates.forEach((data) => {
+    req.user[data] = req.body[data];
+  });
+
+  const uploadQuery = `
+  update uploads
+  set project_name = $3, description = $4
+  where upload_id = $1 and user_id = $2
+  returning *;`;
+
+  try {
+    const upload = await pool.query(uploadQuery, [
+      req.params.id,
+      req.user.user_id,
+      req.user.projectName,
+      req.body.description,
+    ]);
+
+    console.log(upload);
+
+    if (upload.rows.length === 0) throw new Error();
+
+    res.send(upload.rows[0]);
+  } catch (e) {
+    res.send('Unable to Update the Upload');
+  }
+};
+
+module.exports = { getUploadAll, createUpload, deleteUploadAll, viewUpload, deleteUpload, updateUpload };
--- src/routes/uploadRoute.js
+++ src/routes/uploadRoute.js
@@ -3,12 +3,25 @@ const { auth } = require('../helpers/auth');
 
 const router = express.Router();
 
-const { getUploadAll, createUpload, deleteUploadAll } = require('../controllers/upload');
+const {
+  getUploadAll,
+  createUpload,
+  deleteUploadAll,
+  deleteUpload,
+  viewUpload,
+  updateUpload,
+} = require('../controllers/upload');
 
 router.get('/all', auth, getUploadAll);
 
 router.post('/me', auth, createUpload);
 
 router.delete('/all', auth, deleteUploadAll);
+
+router.delete('/me/:id', auth, deleteUpload);
+
+router.get('/me/:id', auth, viewUpload);
+
+router.patch('/me/:id', auth, updateUpload);
 
 module.exports = router;
